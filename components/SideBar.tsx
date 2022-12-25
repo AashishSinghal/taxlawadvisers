85a0155fbf013b631312e646658f4f86d77eadb7
Aashish Singhal <aashish451singhal@gmail.com>
2022-12-25 15:04:45 +0000

    color change

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -25,13 +25,13 @@ const SideBar = () => {
         <GiHamburgerMenu />
       </div>
       <div
-        className={`absolute w-full h-full flex transition-[right] ease-out duration-300 justify-end bg-transparent bg-opacity-0  cursor-pointer top-0 z-50 ${
+        className={`absolute w-full h-full flex transition-[right] ease-out duration-300 justify-end bg-black bg-opacity-10 cursor-pointer top-0 z-50 ${
           isMenuOpen ? "right-0" : "right-[-100%]"
         }`}
         onClick={(e) => handleMennutoggle(e)}
       >
         <div
-          className="flex flex-col h-full w-4/5 bg-primary border-l-2 border cursor-default border-[#62d7a2]"
+          className="flex flex-col h-full w-4/5 border-l-2 border bg-primary-content cursor-default border-[#62d7a2]"
           onClick={(e) => handleClick(e)}
         >
           <div
